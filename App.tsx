3aa2abda58ac0a9e8479a3f16ee57807cd357879
Mac <mac@192.168.0.100>
2024-01-29 16:05:43 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
-import {Provider} from 'react-redux';
-import {store} from './src/redux';
-import {AppBottomTab} from './src/navigation';
-import {NavigationContainer} from '@react-navigation/native';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./src/redux";
+import { AppBottomTab } from "./src/navigation";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <StatusBar barStyle={'dark-content'} />
+      <StatusBar style="dark" />
       <NavigationContainer>
         <AppBottomTab />
       </NavigationContainer>
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  transformIgnorePatterns: [
-    'node_modules/?!((jest-)?react-native|react-redux)',
-  ],
-  setupFiles: ['./setupTests.js'],
-};
